2a0ba8cc28a0bd150226fb83a26411cd11753733
Adrien Colombier <adrien.colombier@epitech.eu>
2019-09-23 00:19:54 +0200

    [ADD] Glow.cpp ECS is pretty fun once done

---
+++ library/source/Glow.cpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2019
+** Bullet.cpp
+** File description:
+** Adrien Colombier
+*/
+
+#include "../include/Glow.hpp"
+
+Glow::Glow()
+{
+    this->setTexture("image/skin/glowSkin.png");
+}
+
+void Glow::draw()
+{
+}
+
+void Glow::autoManage()
+{
+}
